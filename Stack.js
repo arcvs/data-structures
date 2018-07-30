3f355d9f7aefccc7b6c1149a303821dad6014686
Vladimir ArcVS <arctic.vs@gmail.com>
2018-07-30 16:54:33 +0300

    Добавление структуры данных Стэк

---
+++ Stack.js
@@ -0,0 +1,17 @@
+function Stack() {
+
+  var storage = []
+
+  this.push = function(element) {
+    return element
+  }
+
+  this.pop = function() {
+    return element
+  }
+
+  this.pip = function() {
+    return storage
+  }
+
+}
