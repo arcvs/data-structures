201368a6e5af0c7ea888e634e19982c802bc56e6
Vladimir <arctic.vs@gmail.com>
2018-07-30 22:31:43 +0300

    Update Stack.js

--- Stack.js
+++ Stack.js
@@ -3,15 +3,31 @@ function Stack() {
   var storage = []
 
   this.push = function(element) {
-    return element
+
+    storage.push(element)
   }
 
   this.pop = function() {
-    return element
+
+    return storage.pop()
   }
 
   this.pip = function() {
+
     return storage
   }
 
 }
+
+var arr = new Stack()
+
+arr.push(1)
+arr.push(2)
+arr.push(3)
+
+console.log(arr.pip())
+console.log(arr.pop())
+console.log(arr.pop())
+console.log(arr.pop())
+console.log(arr.pop())
+console.log(arr.pip())
